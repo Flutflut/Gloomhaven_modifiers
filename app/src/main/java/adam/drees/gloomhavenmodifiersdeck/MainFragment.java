3102b229dfff7cfdd5b128677dc1eebbf090828d
adrees3 <adrees3@gatech.edu>
2019-01-30 22:55:33 -0600

    Basic Functionality works. Done for the night!

--- app/src/main/java/adam/drees/gloomhavenmodifiersdeck/MainFragment.java
+++ app/src/main/java/adam/drees/gloomhavenmodifiersdeck/MainFragment.java
@@ -1,30 +1,57 @@
 package adam.drees.gloomhavenmodifiersdeck;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment {
     private static final String TAG = "MainFragment";
 
     private ImageView buttonDraw;
+    private Button buttonShuffle;
+    private TextView cardText;
+    private ConstraintLayout cardBack;
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container,false);
 
         buttonDraw = (ImageView) view.findViewById(R.id.buttonDraw);
+        buttonShuffle = (Button) view.findViewById(R.id.buttonShuffle);
+        cardText = (TextView) view.findViewById(R.id.displayCard);
+        cardBack = (ConstraintLayout) view.findViewById(R.id.displayBackground);
+
+
 
         buttonDraw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Card drawnCard;
                 drawnCard = ((MainActivity)getActivity()).playerDeck.draw();
-                v.findViewById(R.id.displayBackground).setBackgroundColor(drawnCard.background);
+                cardText.setText(drawnCard.contents);
+                cardBack.setBackgroundColor(drawnCard.background);
+            }
+        });
+
+
+
+        buttonShuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).playerDeck.shuffle();
+                cardText.setText("");
+                cardBack.setBackgroundColor(Color.parseColor("#FFFFFF"));
             }
         });
 
